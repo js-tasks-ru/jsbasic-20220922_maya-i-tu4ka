03491cfe706cf971ad2ae213b8f892dff891020f
Tu4ka <shchepanowska@gmail.com>
2022-10-20 14:46:48 +0700

    Module 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,16 @@
 function camelize(str) {
-  // ваш код...
+    let arr = str.split("");
+    let newArr = [];
+
+    for(let i = 0; i < arr.length; ++i){
+        if (arr[i] === "-"){
+            let next = arr[++i];
+            let upp = next.toUpperCase();
+
+            newArr.push(upp)
+            arr.splice(i, 1);
+        }
+        newArr.push(arr[i]);
+    }
+    return newArr.join("");
 }
